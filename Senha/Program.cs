3918d768a7ba313eccb75b7df616800ae7bbf5bf
MWLeonel <wmleonel6@gmail.com>
2020-10-01 13:24:41 -0300

    Digite a senha

---
+++ Senha/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Senha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string senha;
+
+            Console.Write("Digite sua senha: ");
+            senha = Console.ReadLine();
+
+            if (senha == "1234abcd")
+            {
+                Console.Write("\n");
+                Console.WriteLine("Acesso Permitido!");
+
+                Console.WriteLine("Pressione ENTER para sair!");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine();
+                Console.WriteLine("Acesso negado");
+
+                Console.WriteLine("Pressione ENTER para sair!");
+                Console.ReadKey();
+            }
+        }
+    }
+}
